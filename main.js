07c838baa20e22342d23c648e2b9103edadfc853
SAPIENT\vragh2 <vraghuvanshi@sapient.com>
2018-06-21 15:05:53 +0530

    updated code

--- main.js
+++ main.js
@@ -1,27 +1,43 @@
-function getData(url) {
-	return new Promise((resolve, reject) => {
-	const req = new XMLHttpRequest();
-	req.open("GET", url, true);
-	req.onload = () => req.status === 200 ? resolve(req.response) : reject(req.statusText);
-	req.send();
+ function createForm(repo) {
+	document.getElementById("repoName").innerHTML = repo;
+	document.getElementById("issueWrapper").style.display="block";
+}
+function createANewIssue() {
+	const { innerHTML } = document.getElementById("repoName");
+	const title = document.getElementById('issueLabel').value;
+	const body = document.getElementById('issueMessage').value;
+	fetch(`https://api.github.com/repos/vishaka27/${innerHTML}/issues`, {
+		method: "post",
+		headers: {
+			"Authorization": "token 09163fdb59f245ee2575f78bdd7792c99fb62fcb"
+		},
+
+		//make sure to serialize your JSON body
+		body: JSON.stringify({ title, body })
+		})
+		.then((res) => { 
+		res.json().then(data => {
+			console.log(data);
+			document.getElementById("successMessage").innerHTML = 'Issue has been created';
+			document.getElementById("successMessage").style.display="block";
+			document.getElementById("issueWrapper").style.display="none";
+		}).catch(e => {
+			document.getElementById("successMessage").innerHTML = 'Opps! something went wrong';
+			console.log(e);
+		})
 	});
-} 
-function createForm() {
-	document.getElementById("issueForm").style.display="block";
 }
-function submitForm () {
-	document.getElementById("issueForm").style.display="none";
-}
+
 function getRepo () {
 	const userName = document.getElementById('vimalForm');
-	getData(`https://api.github.com/users/${userName.value}/repos`)
-	.then(data => {
-		const parsedData = JSON.parse(data);
-		const elements = parsedData.map((item,index) => `<div style="display:flex; justify-content: space-between;"><li style="list-style: none;" class="col-5">
+	fetch(`https://api.github.com/users/${userName.value}/repos`, {headers: {
+        "User-Agent": "09163fdb59f245ee2575f78bdd7792c99fb62fcb"  // Your Github ID or application name
+    }}).then(data => data.json().then(data => {
+		const elements = data.map((item,index) => `<div style="display:flex; justify-content: space-between;"><li style="list-style: none;" class="col-5">
 			${index+1}&nbsp;&nbsp;${item.full_name.split('/')[1]}</li> 
-			<input type="button" class="newIssue" onclick="createForm()" 
+			<input type="button" class="newIssue" onclick="createForm('${item.full_name.split('/')[1]}')" 
 			value="New Issue"></div>`)
 		document.getElementById('content').innerHTML = elements;
 		document.getElementById('user').innerHTML = "List of" + '&nbsp;' + userName.value + '&nbsp;' +"repositories";
-	});
+	}));
 }
